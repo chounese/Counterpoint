0826bc2e2bdb41b17336cfe5ee9a4a2f8de9aeeb
chounese <achou17@amherst.edu>
2015-12-28 21:56:48 -0500

    Refactored code

--- Counterpoint.java
+++ Counterpoint.java
@@ -4,18 +4,18 @@ public class Counterpoint{
 	private HashMap<String, Integer> indexer = new HashMap<>();
 	private String[] noteArray = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 	//True then cp is below false then above
-	private boolean cfAbove;
+	private boolean direction;
 
 	//No args constructor
 	public Counterpoint(){}
 
 	//1 arg constructor
-	public Counterpoint(boolean above){
-		cfAbove = above;
+	public Counterpoint(boolean d){
+		direction = d;
 	}
 
-	public boolean getPosition(){
-		return this.cfAbove;
+	public boolean getDirection(){
+		return this.direction;
 	}
 
 	public HashMap<String, Integer> getIndexer(){
@@ -26,6 +26,106 @@ public class Counterpoint{
 		return this.noteArray;
 	}
 
+
+	public Note findNote(Interval i){
+		//Find number of halfSteps to move
+		int halfSteps = intervalToHalfSteps(i);
+
+		//Direction == true then the cantus firmus is above
+		//Direction == false then the cantus firmus is below
+		if (direction){
+			//Counterpoint is below
+			//higherNote should != null here
+			try{
+				int noteValue = i.getHigherNote().getNoteValue();
+
+			}
+			catch(NullPointerException e){
+				System.out.println("Higher Note is null");
+				e.printStackTrace();
+			}
+			catch(IllegalArgumentException e){
+				System.out.println("Invalid interval " + i.getIntervalType()+i.getInterval());
+				e.printStackTrace();
+			}
+		}
+		else{
+			//Counterpoint is above
+		}
+
+	}
+
+	public int intervalToHalfSteps(Interval i) throws IllegalArgumentException{
+		//Reference check http://www.musictheory.net/lessons/31
+		int halfSteps;
+		String intervalType = i.getIntervalType();
+		int interval = i.getInterval();
+		if (intervalType.equals(Interval.MAJOR)){
+			if (interval == 2 || interval == 3){
+				halfSteps = (interval-1) * 2;
+				return halfSteps;
+			}
+			else if (interval == 6 || interval == 7){
+				halfSteps = (interval-2) * 2 + 1;
+				return halfSteps;
+			}
+		}
+		else if (intervalType.equals(Interval.MINOR)){
+			if (interval == 2 || interval == 3){
+				halfSteps = (interval-1) * 2 - 1;
+				return halfSteps;
+			}
+			else if (interval == 6 || interval == 7){
+				halfSteps = (interval-2) * 2;
+				return halfSteps;
+			}
+		}
+		else if (intervalType.equals(Interval.AUGMENTED)){
+			if (interval == 1 || interval == 2 || interval == 3){
+				halfSteps = (interval-1) * 2 +1;
+				return halfSteps;
+			}
+			else if (interval == 6 || interval == 7){
+				halfSteps = (interval-1) * 2 + 2;
+				return halfSteps;
+			}
+			else if (interval == 4 || interval == 5){
+				halfSteps = (interval-1) * 2;
+				return halfSteps;
+			}
+			else if (interval == 8){
+				halfSteps = (interval-1) * 2 -1;
+				return halfSteps;
+			}
+		}
+		else if (intervalType.equals(Interval.DIMINISHED)){
+			if (interval == 2 || interval == 3 || interval == 4 || interval == 5){
+				halfSteps = (interval-2) * 2;
+				return halfSteps;
+			}
+			else if (interval == 6 || interval == 7 || interval == 8){
+				halfSteps = (interval-2) * 2 - 1;
+				return halfSteps;
+			}
+		}
+		else if (intervalType.equals(Interval.PERFECT)){
+			if (interval == 1){
+				halfSteps = 0;
+				return halfSteps;
+			}
+			else if (interval == 4 || interval == 5){
+				halfSteps = (interval-1) * 2 - 1;
+				return halfSteps;
+			}
+			else if (interval == 8){
+				halfSteps = 12;
+				return halfSteps;
+			}
+		}
+		//If code falls through if/elses then it must be illegal
+		throw new IllegalArgumentException("Illegal Interval");
+		
+	}
 
 
 }
